a32e0b2e836d4eff14c72a8704364522b9d1fe37
Eduard Cucurella <eduard.cucu.cat@gmail.com>
2014-02-23 00:23:32 +0100

    Prod configuration

--- src/ecucurella/SporTiuBundle/Controller/InstallController.php
+++ src/ecucurella/SporTiuBundle/Controller/InstallController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ecucurella\SporTiuBundle\Entity\Club;
 use ecucurella\SporTiuBundle\DataFixtures\SporTiuSchema;
 use ecucurella\SporTiuBundle\DataFixtures\ORM\LoadFixturesData;
+use Doctrine\DBAL\DBALException;
+use PDOException;
 
 class InstallController extends Controller
 {
@@ -16,11 +18,23 @@ class InstallController extends Controller
     public function createSchemaAction() {
         //TODO: Create database if no exist
         //TODO: Alert if any problem
-        $em = $this->getDoctrine()->getManager();
-        SporTiuSchema::dropSchema($em);
-        SporTiuSchema::createSchema($em);
-        $fixture = new LoadFixturesData();
-        $fixture->load($em);
-        return $this->render('ecucurellaSporTiuBundle:Install:schema.html.twig');
+        try {
+            $em = $this->getDoctrine()->getManager();
+            SporTiuSchema::dropSchema($em);
+            SporTiuSchema::createSchema($em);
+            $fixture = new LoadFixturesData();
+            $fixture->load($em);
+            return $this->render('ecucurellaSporTiuBundle:Install:schema.html.twig', array('error' => false));
+        } catch (DBALException $dbal_e) {
+            return $this->render('ecucurellaSporTiuBundle:Install:schema.html.twig', 
+                array(
+                    'error' => 'DBALException: ' . $dbal_e->getMessage()
+                ));  
+        } catch (PDOException $pdo_e) {
+            return $this->render('ecucurellaSporTiuBundle:Install:schema.html.twig', 
+                array(
+                    'error' => 'PDOException: ' . $pdo_e->getMessage()
+                ));
+        }
     }
 }
