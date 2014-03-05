9d715c20aa8cfeff2d7512f681281d6d8835dd47
Eduard Cucurella <eduard.cucu.cat@gmail.com>
2014-03-05 01:50:56 +0100

    last and next games in club info

--- src/ecucurella/SporTiuBundle/Entity/GameRepository.php
+++ src/ecucurella/SporTiuBundle/Entity/GameRepository.php
@@ -33,4 +33,28 @@ class GameRepository extends EntityRepository
         return $query->execute();
     }
 
+    public function findNextGamesbyClub($club)
+    {
+        $dql = "SELECT g FROM ecucurellaSporTiuBundle:Game g
+                WHERE (g.gamestate = 'CALENDAR' OR g.gamestate = 'SCHEDULED')
+                AND ( g.localclub = :club OR g.visitorclub = :club)
+                ORDER BY g.gamedate";
+
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('club', $club);
+        return $query->execute();
+    }
+    
+    public function findLastGamesbyClub($club)
+    {
+        $dql = "SELECT g FROM ecucurellaSporTiuBundle:Game g
+                WHERE (g.gamestate = 'PLAYED' OR g.gamestate = 'SUSPENDED')
+                AND ( g.localclub = :club OR g.visitorclub = :club)
+                ORDER BY g.gamedate DESC";
+
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('club', $club);
+        return $query->execute();
+    }
+
 }
